5ac747c397cbbd87c1ddaa0607fef3f23450c737
郑傲今 <zaj@zhidu.biz>
2018-03-05 16:41:07 +0800

    add opn

--- bin/start.js
+++ bin/start.js
@@ -1,5 +1,7 @@
 const app = require('../server')
+const opn = require('opn')
 const PORT = 8080
 app.listen(PORT,function(){
     console.log("Start：localhost:"+ PORT)
+    opn("http://localhost:"+ PORT)
 })
