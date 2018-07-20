b1badf007da117ae469f58ce04f7ef1fc7901fe0
郑傲今 <zhengaj@shinemo.com>
2018-07-20 15:26:15 +0800

    feat: add normalize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter } from 'react-router-dom'
+import 'normalize'
 import Routers from './routers'
 
 class App extends React.Component {
