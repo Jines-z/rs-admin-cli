69dfb7b088659aac9f310c1c8565bb9036b7fd7c
郑傲今 <zhengaj@shinemo.com>
2018-10-18 19:42:28 +0800

    feat: dll floder and update webpack-cli

--- project.config.js
+++ project.config.js
@@ -9,5 +9,5 @@ module.exports = {
     sourceMap  : NODE_ENV == 'development' ? true : false,
     externals  : {},
     vendor     : ['react', 'react-dom', 'react-router-dom', 'react-loadable', 'mobx', 'mobx-react', 'crypto-js', 'js-cookie']
-    // vendor改变时，需 npm run dll，下一期会做成自动的。
+    // dll 不存在或 vendor 改变时，需 npm run dll，下一期会做成自动的。
 }
