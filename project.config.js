f119aeed471e7abdaccc4fa7f9efe27b93906eb0
郑傲今 <zhengaj@shinemo.com>
2019-03-20 10:14:10 +0800

    feat: Modify pubilcPath and repackage

--- project.config.js
+++ project.config.js
@@ -16,7 +16,7 @@ module.exports = {
     basePath   : __dirname,
     srcDir     : path.resolve(__dirname, 'src'),
     outDir     : path.resolve(__dirname, 'dist'),
-    publicPath : NODE_ENV === 'development' ? './' : 'https://beverle-y.github.io/react-starter-kit/',
+    publicPath : NODE_ENV === 'development' ? './' : 'https://jines-z.github.io/react-starter-kit/',
     esLint     : true,
     vendor     : ['react', 'react-dom', 'react-router-dom', 'react-loadable', 'mobx', 'mobx-react', 'crypto-js', 'js-cookie', 'flyio'],
     // 当 (dll不存在) (vendor被改变) (包的版本被更换) 时，请 npm run dll。
