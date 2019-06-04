f1d7a08021281e6c03134e6befcdd372bce09be6
郑傲今 <zhengaj@shinemo.com>
2019-06-04 10:30:09 +0800

    feat: Relative path

--- project.config.js
+++ project.config.js
@@ -2,13 +2,14 @@
 
     ** 配置文件 **
 
-    1、theme  ：antd主题配置
-    2、html   ：在 index.html 中，使用 <%= htmlWebpackPlugin.options.xxx %> 来访问 html 中的属性，可扩展
-    3、global ：全局变量，使用时前后加“__”，字母变大写，例如 __ENV__，可扩展
-    4、branch ：当前所在分支，可用于打包时判断（测试/线上）环境，等等
-    5、proxy  ：本地代理，请在 proxy.config.js 中配置你需要代理的地址
-    6、env    ：环境变量，开发环境为 development，生产环境为 production
-    7、vendor ：用于生成 dll 包，当 (dll不存在) (vendor被改变) (包的版本被更换) 时，请 npm run dll
+    1、theme    ：主题配置
+    2、html     ：在 index.html 中，使用 <%= htmlWebpackPlugin.options.xxx %> 来访问 html 中的属性，可扩展
+    3、global   ：全局变量，使用时前后加“__”，字母变大写，例如 __ENV__，可扩展
+    4、branch   ：所在分支，可用于打包时判断（测试/线上）环境，等等
+    5、proxy    ：本地代理，请在 proxy.config.js 中配置你需要代理的地址
+    6、env      ：环境变量，开发环境为 development，生产环境为 production
+    7、relative : 相对路径，开启时，publicPath 将变为 './'，dist 目录下所有文件在同一层级，可直接访问 index.html
+    8、vendor   ：用于生成 dll 包，当 (dll不存在) (vendor被改变) (包的版本被更换) 时，请 npm run dll
 
    ====================================================== */
 const NODE_ENV = process.env.NODE_ENV || 'development'
@@ -43,5 +44,6 @@ module.exports = {
     dllDir     : path.resolve(__dirname, 'dll'),
     publicPath : NODE_ENV === 'development' ? './' : 'https://jines-z.github.io/react-starter-kit/',
     esLint     : true,
+    relative   : false,
     vendor     : ['react', 'react-dom', 'react-router-dom', 'react-loadable', 'mobx', 'mobx-react', 'crypto-js', 'js-cookie', 'flyio'],
 }
